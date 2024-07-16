706f4e158a70f587632814923481912b407eeb79
shireeshatn <146500562+shireeshatn@users.noreply.github.com>
2024-07-16 05:10:52 +0000

    Updated the docker-compose to support latest changes

--- app/config.py
+++ app/config.py
@@ -1,9 +1,11 @@
 # config.py
+import os
+
 
 class Config:
     # Flask configuration
     SECRET_KEY = 'dermai'
-    
+
     # Database configuration
-    SQLALCHEMY_DATABASE_URI = 'postgresql://group5:dermai@localhost/dermai'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", 'postgresql://group5:dermai@localhost/dermai')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
